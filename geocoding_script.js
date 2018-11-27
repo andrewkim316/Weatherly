9737666a470f286a8a3524b2d72dadc3c7646415
Andrew Kim <andrewkim2021@u.northwestern.edu>
2018-11-27 03:02:06 -0600

    accessing different data from api

--- geocoding_script.js
+++ geocoding_script.js
@@ -54,42 +54,55 @@ function initMap() {
         get_weather_AX();
     });
 }
-
+var weather_DS = ["DS"];
 function get_weather_DS(){
     let weather_key = "***REMOVED***";
     let weather_api = `https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${weather_key}/${lat},${lng}`;
     fetch(weather_api)
     .then(resp => resp.json())
     .then(function(data){
-        window.alert(data.daily.summary);
+        weather_DS.push(data.currently.temperature);
+        weather_DS.push(data.currently.summary);
+        window.alert(weather_DS.toString());
     })
     .catch(function(error){
         window.alert(error);
     });
 }
 
+var weather_OWM = ["OWM"];
 function get_weather_OWM(){
     let weather_key = "***REMOVED***";
     let weather_api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&APPID=${weather_key}`
     fetch(weather_api)
     .then(resp => resp.json())
     .then(function(data){
-        window.alert((data.main.temp)-273);
+        weather_OWM.push(FtoC((data.main.temp)-273));
+        weather_OWM.push(data.weather[0].main);
+        window.alert(weather_OWM.toString());
     })
     .catch(function(error){
         window.alert(error);
     });
 }
 
+var weather_AX = ["AX"];
 function get_weather_AX(){
     let weather_key = "***REMOVED***";
     let weather_api = `http://api.apixu.com/v1/current.json?key=${weather_key}&q=${lat},${lng}`;
     fetch(weather_api)
     .then(resp => resp.json())
     .then(function(data){
-        window.alert(data.current.temp_f);
+        weather_AX.push(data.current.temp_f);
+        weather_AX.push(data.current.condition.text);
+        window.alert(weather_AX.toString());
     })
     .catch(function(error){
         window.alert(error);
     });
+}
+
+function FtoC(t,b){
+    if(b) return (t-32) * (5/9);
+    return (t * (9/5)) + 32;
 }
